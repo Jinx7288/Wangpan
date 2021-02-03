d61aad3be72eae10fce062eebeb7570cea5bdcfb
Jinx7288 <jinx.drx@gmail.com>
2021-02-04 00:43:15 +0800

    fix something

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+"use strict"
+new Vue({
+    el:"#login",
+    data(){
+        return {
+            stateTg:true,
+            userid:'',
+            password:'',
+            adduser:{
+                addid:'',
+                addemail:'',
+                addpw:'',
+                addpw1:''
+            }
+           
+        }  
+    },
+    methods:{
+        logIn:function() {
+
+        },
+        signUp:function() {
+
+        },
+    }
+})
