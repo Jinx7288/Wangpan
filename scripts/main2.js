6875eb4c3421c9c939744fab1d62c44fcc669de6
Jinx7288 <jinx.drx@gmail.com>
2021-02-04 11:53:37 +0800

    delete something

---
+++ scripts/main2.js
@@ -0,0 +1,22 @@
+"use strict"
+new Vue({
+    el:"#app",
+    data(){
+        return {
+            userState:false,
+            userInfo:{
+                headUrl:'',
+                userId:'',
+                userSpace:'',
+                fileTree:{
+                    
+                }
+            },
+
+           
+        }  
+    },
+    methods:{
+
+    }
+})
